1d3b5f41282642cef7d1446d4b940f19042cb08b
Umar Ahmed <8302959+umar-ahmed@users.noreply.github.com>
2018-05-06 00:08:26 -0400

    Add Firebase integration (#1)
    Add Firebase integration and sign in code

---
+++ lib/sign_in.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final googleSignIn = new GoogleSignIn();
+
+Future<Null> signIn() async {
+    GoogleSignInAccount user = googleSignIn.currentUser;
+    if (user == null)
+        user = await googleSignIn.signInSilently();
+    if (user == null)
+        await googleSignIn.signIn();
+}
